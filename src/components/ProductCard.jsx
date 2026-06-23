076e11481be36556ef37494fd5400bae720807d8
RaeChoe <picasomati@gmail.com>
2026-06-23 17:01:22 +0900

    feat: ProductCard.jsx 수정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,14 +2,14 @@ import styles from "./ProductCard.module.css";
 
 function ProductCard({ data }) {
   const cardList = data.map(d => (
-    <ul key={d.id}>
-      <li>{d.name}</li>
-      <li>{d.price}원</li>
-      <li>{d.brand}</li>
-    </ul>
+    <article key={d.id} className={styles.card}>
+      <h3 className={styles.name}>{d.name}</h3>
+      <p className={styles.price}>{d.price.toLocaleString()}원</p>
+      <p className={styles.brand}>{d.brand}</p>
+    </article>
   ));
 
-  return <article className={styles.card}>{cardList}</article>;
+  return <>{cardList}</>;
 }
 
 export default ProductCard;
