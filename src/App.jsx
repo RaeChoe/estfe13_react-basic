8a588f6a6a48d94d15bbe3cf37733053fa9f8564
RaeChoe <picasomati@gmail.com>
2026-06-23 15:46:41 +0900

    feat: 컴폰너트 분리, 클릭 이벤트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,72 @@
 import "./App.css";
-
-// Myheader 컴포넌트
-function Myheader() {
-  return (
-    <header>
-      <h1 className="logo">프론트엔드 개발자</h1>
-      <p>기본언어인 html, css, javascript부터 학습합니다.</p>
-    </header>
-  );
-}
-
-// Nav 컴포넌트
-function Nav() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="">UI/UX 개발</a>
-        </li>
-        <li>
-          <a href="">재사용이 가능한 UI 개발</a>
-        </li>
-        <li>
-          <a href="">애니메이션 구현</a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-function MyArticle() {
-  return (
-    <section>
-      <article>
-        <h2>HTML</h2>
-        <p>Hypertext Markup Language</p>
-      </article>
-    </section>
-  );
-}
+import Myheader from "./components/Myheader";
+import Nav from "./components/Nav";
+import MyArticle from "./components/MyArticle";
+import { useState } from "react";
 
 function App() {
+  console.log("App render");
+  const [id, setId] = useState(1);
+  const [mode, setMode] = useState("welcome");
+  const [subject, setSubject] = useState({
+    title: "프론트엔드 개발자",
+    desc: "기본언어인 html, css, javascript부터 학습합니다.",
+  });
+  const [content, setContent] = useState([
+    { id: 1, title: "UI/UX 개발", desc: "사용자 경험을 고려한 직관적이고 반응성 높은 화면 구현" },
+    {
+      id: 2,
+      title: "재사용이 가능한 UI 개발",
+      desc: "컴포넌트 기반으로 동일한 UI를 효율적으로 재사용 가능",
+    },
+    { id: 3, title: "애니메이션 구현", desc: "상태 변화에 따른 자연스럽고 동적인 화면 효과 구현" },
+  ]);
+
+  const welcome = { title: "welcome", desc: "Welcome to React" };
+
+  let _title = null;
+  let _desc = null;
+
+  if (mode === "welcome") {
+    _title = welcome.title;
+    _desc = welcome.desc;
+  } else if (mode === "read") {
+    const selected = content.find(c => c.id === id);
+
+    if (selected) {
+      _title = selected.title;
+      _desc = selected.desc;
+    }
+  }
+
   return (
     <>
-      <Myheader />
-      <Nav />
-      <MyArticle />
+      <Myheader
+        title={subject.title}
+        desc={subject.desc}
+        onChangeMode={() => {
+          setMode("welcome");
+        }}
+      />
+      {/* <header>
+        <h1
+          className="logo"
+          onClick={() => {
+            setMode("welcome");
+          }}
+        >
+          {subject.title}
+        </h1>
+        <p>{subject.desc}</p>
+      </header> */}
+      <Nav
+        data={content}
+        onChangeMode={id => {
+          setMode("read");
+          setId(id);
+        }}
+      />
+      <MyArticle title={_title} desc={_desc} />
     </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+function Nav({ data, onChangeMode }) {
+  console.log("Nav render");
+  const lists = data.map(d => (
+    <li key={d.id}>
+      <a
+        href={`/${d.id}`}
+        data-id={d.id}
+        onClick={e => {
+          e.preventDefault();
+          onChangeMode(Number(e.target.dataset.id));
+        }}
+      >
+        {d.title}
+      </a>
+    </li>
+  ));
+  // lists에 출력할 코드 생성
+
+  return (
+    <nav>
+      <ul>{lists}</ul>
+    </nav>
+  );
+}
+
+export default Nav;
